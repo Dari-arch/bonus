7af035ede1190d7f77215c1462759285111fcf53
Dari-arch <dariya.05.07@icloud.com>
2024-10-20 00:36:17 +0600

    Create 3.C

---
+++ 3.C
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int c=0,p=0,n=0;
+    for (int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        if(x==0){ ++c;
+        }
+        else if(x>0) {   ++p;}
+        ese{
+            ++n;
+        }
+    cout<<c<<" " <<p<<" "<<n<<endl;
+
+    return 0;
+}
